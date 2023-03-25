34fe7cfd1822e91b31493dfb1975f33dc024a005
YİĞİTCAN ÇIRPAN <19070006058@stu.yasar.edu.tr>
2023-03-25 15:59:17 +0300

    identity

---
+++ DevOpsProject/DataAccess/DevContext.cs
@@ -0,0 +1,9 @@
+﻿using DevOpsProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DevOpsProject.DataAccess
+{
+    public class DevContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+    }
+}
